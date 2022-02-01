a827716bbeef824b3e1ace5eb733b96496518cd9
Shweta <shweta.sim@gmail.com>
2022-02-01 13:15:23 +0100

    update

---
+++ bilder.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Legitimerade Tandläkare för traditionell & akut tandvård. Även Tandblekning, rynkbehandling, botox m.m. I Stockholm finns vi i Älvsjö, Huddinge, Farsta & Enskede." />
+<meta name="keywords" content="Legitimerade Tandläkare i Älvsjö, Huddinge, Farsta & Enskede, Stockholm. Akut tandvård, traditionell tandvård, Estetisk tandvård, botox och läppförstoring." />
+<meta name="robots" content="index,follow,noodp,noydir" />
+<meta name="author" content="">
+<title>Akut tandvård, Tandläkare i Älvsjö, Huddinge, Farsta & Enskede | Bandhagen Dental Care</title>
+<link rel="icon" type="image/png" href="images/favicon.png">
+<link href="inner-pages/css/bootstrap.min.css" rel="stylesheet">
+<link href="inner-pages/fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="inner-pages/css/main.css" rel="stylesheet">
+<link href="css/template.css" rel="stylesheet">
+<link href="inner-pages/fonts/css/fontello.css" rel="stylesheet">
+
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<!-- Facebook Pixel Code -->
+
+<script>
+
+  !function(f,b,e,v,n,t,s)
+
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+
+  n.queue=[];t=b.createElement(e);t.async=!0;
+
+  t.src=v;s=b.getElementsByTagName(e)[0];
+
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+
+  'https://connect.facebook.net/en_US/fbevents.js');
+
+  fbq('init', '441259379973506');
+
+  fbq('track', 'PageView');
+
+</script>
+<noscript>
+<img height="1" width="1" style="display:none"
+
+  src="https://www.facebook.com/tr?id=441259379973506&ev=PageView&noscript=1"
+
+/>
+</noscript>
+
+<!-- End Facebook Pixel Code -->
+
+<script>
+
+  fbq('track', 'ViewContent');
+
+</script>
+</head>
+<body id="innerpage">
+<div class="wrap ccm-page"> 
+  <!-- Header Start -->
+  <?php include("header.php"); ?>
+  <!-- Header Ends --> 
+  
+  <!-- Breadcrumbs Start -->
+  <div class="breadcrumb">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12"> Du är här : <span class="active">Bilder</span> </div>
+      </div>
+    </div>
+  </div>
+  <!-- Breadcrumbs Ends --> 
+  
+  <!-- Main Start -->
+  <div class="main" id="main">
+    <section class="nopadding">
+    <div class="container" id="forebyggande"> <a name="forebyggande"></a>
+      <div class="row treatment-list first">
+            <div class="col-md-12 col-sm-12 text-center"> 
+                <h4>Coming Soon</h4>
+            </div>
+      </div>
+ 
+  </div>
+  <!-- Main Ends --> 
+  
+  <!-- Footer Start -->
+  <?php include("footer.php"); ?>
+  <!-- Footer Ends --> 
+</div>
+<!-- Core Javascripts --> 
+<script type="text/javascript" src="inner-pages/js/jquery.js"></script> 
+<script type="text/javascript" src="inner-pages/js/bootstrap.min.js"></script> 
+=
+
+<script type="text/javascript" src="js/navigation.js"></script>
+<script type="text/javascript" src="inner-pages/js/jquery.js"></script> 
+<script type="text/javascript" language="javascript" src="inner-pages/js/bookmarkscroll.js"></script> 
+<script type="text/javascript" src="inner-pages/js/offcanvas.js"></script>
+</body>
+</html>
